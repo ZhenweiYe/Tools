47a166c1e0bd722069a4893af187aec703d69b20
chendeqing <chendeqing@xianjincard.com>
2017-03-28 17:45:31 +0800

    add trim for pretty docstring display

--- qcutils.py
+++ qcutils.py
@@ -7,15 +7,39 @@ import datetime as dt
 
 def conv_long_to_dtm(dt_l):
     '''
-    convert long to datetime
+    Convert long to datetime
     >>> conv_long_to_dtm(1486456336)
-    Outputs:
     2017-02-07 16:32:16
-
     '''
     return dt.datetime.fromtimestamp(dt_l)
-
-
+    
+    
+def trim(docstring):
+    if not docstring:
+        return ''
+    # Convert tabs to spaces (following the normal Python rules)
+    # and split into a list of lines:
+    lines = docstring.expandtabs().splitlines()
+    # Determine minimum indentation (first line doesn't count):
+    indent = sys.maxint
+    for line in lines[1:]:
+        stripped = line.lstrip()
+        if stripped:
+            indent = min(indent, len(line) - len(stripped))
+    # Remove indentation (first line is special):
+    trimmed = [lines[0].strip()]
+    if indent < sys.maxint:
+        for line in lines[1:]:
+            trimmed.append(line[indent:].rstrip())
+    # Strip off trailing and leading blank lines:
+    while trimmed and not trimmed[-1]:
+        trimmed.pop()
+    while trimmed and not trimmed[0]:
+        trimmed.pop(0)
+    # Return a single string:
+    return '\n'.join(trimmed)
+    
+    
 def main():
     print(conv_long_to_dtm(1486456336))
 
